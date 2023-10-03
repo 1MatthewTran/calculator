a2cc05ee3f19d9de81469f9f288a430ff69a270e
1MatthewTran <1matthewtran@gmail.com>
2023-10-03 10:05:52 -0400

    create operate function that takes an operator and two functions and applies the operation

--- calculator.js
+++ calculator.js
@@ -8,5 +8,22 @@ function multiply(a,b){
     return a*b;
 }
 function divide(a,b){
+    if(b===0){
+        return undefined
+    }
     return a/b;
+}
+function operate(operator, a, b){
+    switch(operator){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a,b);
+        case 'x':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+        default:
+            return "Invalid Operator";
+    }
 }
