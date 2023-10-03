06c4fdc2117d4bf792ea9d243ee7e0bf478463bb
1MatthewTran <1matthewtran@gmail.com>
2023-10-03 15:21:45 -0400

    add functions to add functionality to buttons and display calulations

--- calculator.js
+++ calculator.js
@@ -2,13 +2,18 @@ const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
 
 let num1 = '';
 let num2 = '';
-let a;
-let b;
-let operator;
-let isNum1 = true;
+let operator = '';
+
+function reset(){
+    num1 = '';
+    num2 = '';
+    operator = '';
+    display.textContent = '';
+}
 
 function add(a,b){
     return a+b;
@@ -20,9 +25,6 @@ function multiply(a,b){
     return a*b;
 }
 function divide(a,b){
-    if(b===0){
-        return undefined
-    }
     return a/b;
 }
 function operate(operator, a, b){
@@ -42,7 +44,54 @@ function operate(operator, a, b){
 
 numberButtons.forEach(button => {
     button.addEventListener('click', function(e){
-        display.textContent += button.textContent;
+        if(!operator){
+            num2 = null;
+        }
+        concat = num1.toString() + button.textContent;
+        display.textContent = concat;
+        num1 = parseFloat(display.textContent);
     })
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', function(e){
+        if(!num2){
+            num2 = num1;
+            num1 = '';
+        }
+        else if(num1===''){
+        }
+        else{
+            if(operator==='/' && num1===0){
+                alert("DIVIDING BY 0? SHAME ON YOU");
+                reset();
+                return;
+            }
+            rslt = operate(operator, num2, num1);
+            display.textContent = rslt;
+            num2 = rslt;
+            num1 = '';
+        }
+        operator = button.textContent;
+    })
+});
+
+equalsButton.addEventListener('click', function(e){
+    if(operator && (num1 || num1===0)){
+        rslt = operate(operator, num2, num1)
+        if(operator==='/' && num1===0){
+            alert("DIVIDING BY 0? SHAME ON YOU");
+            reset();
+            return;
+        }
+        display.textContent = rslt;
+        num2 = rslt;
+        num1 = '';
+        operator = '';
+    }
+});
+
+clearButton.addEventListener('click', function(e){
+    reset();
 });
 
